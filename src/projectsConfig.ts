31e2a3e2e6984396cc62edfeecdf0121757539f9
cadu <caduromanow@hotmail.com>
2026-09-08 10:55:16 -0300

    feat: new repo

--- src/projectsConfig.ts
+++ src/projectsConfig.ts
@@ -1,12 +1,16 @@
 export const featuredRepositories = [
+
+   {
+    repo: "caduhroman/sales-analysis",
+    featured: true,
+  },
+
   {
     repo: "caduhroman/stockmais",
     featured: true,
   },
 
-  {
-    repo: "caduhroman/parkercontrol",
-    featured: true,
-  },
+ 
+
 ];                     
 
